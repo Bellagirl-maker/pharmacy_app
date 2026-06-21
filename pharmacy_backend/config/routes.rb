eee6ae22d6203454c211fa968ab7dcae197e0c8c
Isabella <isabellaotoo25@gmail.com>
2026-06-21 14:50:08 +0000

    Generate order channels

--- pharmacy_backend/config/routes.rb
+++ pharmacy_backend/config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "orders/create"
   resources :medicines, only: [:index]
   resources :orders, only: [:create, :update]
+
+  mount ActionCable.server => '/cable'
 end
