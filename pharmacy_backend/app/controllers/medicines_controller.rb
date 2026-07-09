c6599892c44eda33b214057240d302ec8fc34af2
Isabella <isabellaotoo25@gmail.com>
2026-07-09 23:02:18 +0000

    Update MedicinesController

--- pharmacy_backend/app/controllers/medicines_controller.rb
+++ pharmacy_backend/app/controllers/medicines_controller.rb
@@ -1,13 +1,17 @@
 class MedicinesController < ApplicationController
+  # GET /medicines
   # GET /medicines?search=Vit
   def index
     if params[:search].present?
-      # Case-insensitive SQL search matching any part of the name
-      @medicines = Medicine.where("LOWER(name) LIKE ?", "%#{params[:search].downcase}%")
+      # Case-insensitive SQL search matching any part of the name with eager loading
+      @medicines = Medicine.includes(:batches).where("LOWER(name) LIKE ?", "%#{params[:search].downcase}%")
     else
-      @medicines = Medicine.all
+      @medicines = Medicine.includes(:batches).all
     end
 
-    render json: @medicines
+    # Render JSON including the associated nested batches relation
+    render json: @medicines.to_json(include: { 
+      batches: { only: [:batch_number, :quantity, :expiry_date] } 
+    })
   end
 end
