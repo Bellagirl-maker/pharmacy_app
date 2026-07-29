f993d0096dc5338f8b96b46dce18356d53b940e8
Isabella <isabellaotoo25@gmail.com>
2026-07-29 23:01:02 +0000

    Allow Vercel frontend origin in CORS config

--- pharmacy_backend/config/initializers/cors.rb
+++ pharmacy_backend/config/initializers/cors.rb
@@ -1,11 +1,11 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173' # Must be the exact frontend URL, do not use '*'
+    origins 'http://localhost:5173', 'https://pharmacy-app-6edc.vercel.app'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true # <-- This must be set to true
+      credentials: true
   end
 end
