32a2b1931b494b33821711201d82906efd6d7cf2
Isabella <isabellaotoo25@gmail.com>
2026-09-06 23:29:53 +0000

    Add demo frontend URL to CORS config

--- pharmacy_backend/config/initializers/cors.rb
+++ pharmacy_backend/config/initializers/cors.rb
@@ -2,7 +2,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:5173', 
             'https://pharmacy-app-6edc.vercel.app',
-            'https://pharmacy-frontend-krpu.onrender.com'
+            'https://pharmacy-frontend-krpu.onrender.com',
+            'https://pharmacy-demo-frontend.onrender.com'
 
     resource '*',
       headers: :any,
